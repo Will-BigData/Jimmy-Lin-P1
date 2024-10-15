9e6cc0d7c13fc17d622044876e5eb40dfacf6bc8
XeenProof <72842381+XeenProof@users.noreply.github.com>
2024-10-15 15:56:56 -0400

    connection setup

---
+++ connection.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+host = os.getenv('HOST')
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
+database=os.getenv('DATABASE')
+port=os.getenv('PORT', 3306)
+print(database)
+
+connection = mysql.connector.connect(
+    host = host,
+    user = user,
+    password = password,
+    database = database,
+    port=port
+)
+
+if connection.is_connected():
+    print("Connected to MySQL database")
+
