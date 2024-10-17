170537f539ab7b152c6fa7c8175a8ab14665ae71
XeenProof <72842381+XeenProof@users.noreply.github.com>
2024-10-17 15:44:39 -0400

    UserDAO Started

--- DAO/UserDAO.py
+++ DAO/UserDAO.py
@@ -1,4 +1,38 @@
-
+from mysql.connector import Error
+from connections.ConnectionUtil import ConnectionUtil
 
 class UserDAO:
-    pass
+    def get_user_by_id(self, id):
+        try:
+            conn = ConnectionUtil.get_connection()
+            cursor = conn.cursor(dictionary=True)
+            sql = "SELECT * FROM users WHERE id = %s;"
+            cursor.execute(sql,(id,))
+            result = cursor.fetchone()
+            if result:
+                return result
+            else:
+                print("No user found")
+                return None
+        except Error as e:
+            print("An error has occured while fetching the item")
+            print(e)
+            pass
+        finally:
+            cursor.close()
+    
+    def get_all_users(self):
+        try:
+            conn = ConnectionUtil.get_connection()
+            cursor = conn.cursor(dictionary=True)
+            sql = "SELECT * FROM users;"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            if result:
+                return result
+            else:
+                print("No user found")
+                return None
+        except Error:
+            print("An error has occured while fetching the item")
+            pass
