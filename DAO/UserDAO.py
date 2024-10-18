298dcc374f689256e3466f595a4a6a425b9615ad
XeenProof <72842381+XeenProof@users.noreply.github.com>
2024-10-18 11:31:36 -0400

    user dao completed

--- DAO/UserDAO.py
+++ DAO/UserDAO.py
@@ -21,6 +21,25 @@ class UserDAO:
         finally:
             cursor.close()
     
+    def get_user_by_email(self, email):
+        try:
+            conn = ConnectionUtil.get_connection()
+            cursor = conn.cursor(dictionary=True)
+            sql = "SELECT * FROM users WHERE email = %s;"
+            cursor.execute(sql,(email,))
+            result = cursor.fetchone()
+            if result:
+                return result
+            else:
+                print("No user found")
+                return None
+        except Error as e:
+            print("An error has occured while fetching the item")
+            print(e)
+            pass
+        finally:
+            cursor.close()
+    
     def get_all_users(self):
         try:
             conn = ConnectionUtil.get_connection()
@@ -33,6 +52,42 @@ class UserDAO:
             else:
                 print("No user found")
                 return None
+        except Error:
+            print("An error has occured while fetching the item")
+            pass
+
+    def create_user(self, user):
+        try:
+            conn = ConnectionUtil.get_connection()
+            cursor = conn.cursor(dictionary=True)
+            sql = "INSERT INTO users (username, email, password) VALUES (%s, %s, %s);"
+            cursor.execute(sql,(user['username'], user['email'], user['password']))
+            conn.commit()
+            print("User created successfully.")
+        except Error:
+            print("An error has occured while fetching the item")
+            pass
+
+    def update_user(self, id, user):
+        try:
+            conn = ConnectionUtil.get_connection()
+            cursor = conn.cursor(dictionary=True)
+            sql = "UPDATE users SET username = %s, email = %s, password = %s WHERE id = %s;"
+            cursor.execute(sql,(user['username'], user['email'], user['password'], id))
+            conn.commit()
+            print("User updated successfully.")
+        except Error:
+            print("An error has occured while fetching the item")
+            pass
+
+    def delete_user_by_id(self, id):
+        try:
+            conn = ConnectionUtil.get_connection()
+            cursor = conn.cursor(dictionary=True)
+            sql = "DELETE FROM users WHERE id = %s;"
+            cursor.execute(sql,(id,))
+            conn.commit()
+            print("User deleted successfully.")
         except Error:
             print("An error has occured while fetching the item")
             pass
