7845737392e7fc4a0e0fbe4ed6d53b7aaea9ea0c
XeenProof <72842381+XeenProof@users.noreply.github.com>
2024-10-21 16:28:35 -0400

    DAO updates

--- DAO/OrderDAO.py
+++ DAO/OrderDAO.py
@@ -76,15 +76,16 @@ class OrderDAO:
             cursor.execute(sql,(order['user_id'], order['item_id'], order['amount']))
             conn.commit()
             print("Order created successfully.")
+            return True
         except Error:
             print("An error has occured while fetching the item")
-            pass
+            return False
 
     def update_order(self, id, order):
         try:
             conn = ConnectionUtil.get_connection()
             cursor = conn.cursor(dictionary=True)
-            sql = "UPDATE orders SET amount = %s WHERE id = %s;"
+            sql = "UPDATE orders SET amount = %s WHERE id = %s AND commited = FALSE;"
             cursor.execute(sql,(order['amount'], id))
             conn.commit()
             print("Order updated successfully.")
@@ -96,10 +97,29 @@ class OrderDAO:
         try:
             conn = ConnectionUtil.get_connection()
             cursor = conn.cursor(dictionary=True)
-            sql = "DELETE FROM orders WHERE id = %s;"
+            sql = "DELETE FROM orders WHERE id = %s AND commited = FALSE;"
             cursor.execute(sql,(id,))
             conn.commit()
             print("Order deleted successfully.")
         except Error:
             print("An error has occured while fetching the item")
             pass
+
+    def get_order_price(self, user_id, name=False): #WIP
+        try:
+            conn = ConnectionUtil.get_connection()
+            cursor = conn.cursor(dictionary=True)
+            sql = "SELECT * FROM orders JOIN items ON orders.item_id = items.id WHERE user_id = %s;"
+            cursor.execute(sql,(user_id,))
+            result = cursor.fetchone()
+            if result:
+                return result
+            else:
+                print("No user found")
+                return []
+        except Error as e:
+            print("An error has occured while fetching the item")
+            print(e)
+            pass
+        finally:
+            cursor.close()
